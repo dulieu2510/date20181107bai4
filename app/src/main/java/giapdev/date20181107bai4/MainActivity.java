6edc6a58d6db7d5b60f16946d13cb851cc3c1f5b
dulieu2510 <giaptg@gmail.com>
2018-11-08 21:41:49 +0700

    1)develop listview

--- app/src/main/java/giapdev/date20181107bai4/MainActivity.java
+++ app/src/main/java/giapdev/date20181107bai4/MainActivity.java
@@ -2,6 +2,10 @@ package giapdev.date20181107bai4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lv1 = (ListView) findViewById(R.id.xlv1);
+        ArrayList<String> arr = new ArrayList<String>();
+        arr.add("list1");
+        arr.add("list2");
+        arr.add("list3");
+        ArrayAdapter art = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,arr);
+        lv1.setAdapter(art);
     }
 }
